267ac97aef1bc61173f163ae1de26dc8522cc20e
Jannis Rauschke <jannis.rauschke@ipt.ch>
2026-04-14 09:38:05 +0200

    Add unit test

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,7 +1,13 @@
-import { render} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
     render(<Counter />);
-    // Add the test logic below
+
+    const button = screen.getByRole("button", { name: "count is 0" });
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    expect(screen.getByRole("button", { name: "count is 1" })).toBeInTheDocument();
 });
